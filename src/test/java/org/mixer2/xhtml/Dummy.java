2d8a5dfe6cf672b2140c16eef6da0ec60dc1ff90
nabedge <nabedge@gmail.com>
2012-12-08 10:44:46 +0900

    fix typo

---
+++ src/test/java/org/mixer2/xhtml/Dummy.java
@@ -0,0 +1,27 @@
+package org.mixer2.xhtml;
+
+import java.io.IOException;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mixer2.xhtml.TagCreator.*;
+
+@SuppressWarnings("unused")
+public class Dummy {
+
+    @AfterClass
+    public static void afterClass() {
+    }
+
+    @Before
+    public void init() throws IOException {
+    }
+
+    @Test
+    public void test() throws Exception {
+        //
+    }
+
+}
