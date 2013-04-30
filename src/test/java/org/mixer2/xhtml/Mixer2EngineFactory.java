b7d1e2718922095e209f68fa7e02d10c6525de97
nabedge <nabedge@gmail.com>
2013-04-30 17:47:51 +0900

    add singleton factory for test.

---
+++ src/test/java/org/mixer2/xhtml/Mixer2EngineFactory.java
@@ -0,0 +1,15 @@
+package org.mixer2.xhtml;
+
+import org.mixer2.Mixer2Engine;
+
+public class Mixer2EngineFactory {
+
+    private Mixer2EngineFactory() {}
+    
+    private static final Mixer2Engine m2e = new Mixer2Engine();
+    
+    public static Mixer2Engine getInstance() {
+        return m2e;
+    }
+    
+}
