727a2bee461e47e82df99e5d2d8b034860411b9f
nabedge <nabedge@gmail.com>
2013-07-12 14:21:46 +0900

    add performance test

---
+++ src/test/java/org/mixer2/xhtml/ToStringPerformanceTest.java
@@ -0,0 +1,89 @@
+package org.mixer2.xhtml;
+
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+import org.apache.commons.lang.time.StopWatch;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mixer2.Mixer2Engine;
+import org.mixer2.jaxb.xhtml.Html;
+import org.mixer2.util.CastUtil;
+
+public class ToStringPerformanceTest {
+
+    private String templateFileName = "sample-xhtml1-transitional.html";
+    private String templateFilePath;
+    private static Mixer2Engine m2e = Mixer2EngineSingleton.getInstance();
+
+    private int loop = 1000;
+
+    @BeforeClass
+    public static void beforeClass() {
+    }
+
+    @Before
+    public void before() {
+        templateFilePath = getClass().getResource(templateFileName).toString();
+        String osname = System.getProperty("os.name");
+        if(osname.indexOf("Windows")>=0){
+            templateFilePath = templateFilePath.replaceFirst("file:/", "");
+        } else {
+            templateFilePath = templateFilePath.replaceFirst("file:", "");
+        }
+    }
+
+    @Test
+    public void loopToString() throws Exception {
+        Html html = m2e.loadHtmlTemplate(new File(templateFilePath));
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        for (int i=0; i<loop; i++) {
+            html.toString();
+        }
+        stopWatch.stop();
+        System.out.println("   normal toString(): loop= " + loop + ", time(msec)= " + stopWatch.getTime());
+    }
+
+    @Test
+    public void loopToStringByReflectionToStringBuilder() throws Exception {
+        Html html = m2e.loadHtmlTemplate(new File(templateFilePath));
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        for (int i=0; i<loop; i++) {
+            toStringByReflectionToStringBuilder(html);
+        }
+        stopWatch.stop();
+        System.out.println("ReflectionToString(): loop= " + loop + ", time(msec)= " + stopWatch.getTime());
+    }
+
+    private String toStringByReflectionToStringBuilder(Object bean) {
+        return (new ReflectionToStringBuilder(bean,
+                ToStringStyle.MULTI_LINE_STYLE) {
+            public ToStringBuilder append(String fieldName, Object obj) {
+                if (obj != null) {
+                    if (obj instanceof List<?>) {
+                        List<?> list = CastUtil.cast(obj);
+                        if (0 < list.size()) {
+                            super.append(fieldName, obj);
+                        }
+                    } else if (obj instanceof Map<?, ?>) {
+                        Map<?, ?> map = CastUtil.cast(obj);
+                        if (0 < map.size()) {
+                            super.append(fieldName, obj);
+                        }
+                    } else {
+                        super.append(fieldName, obj);
+                    }
+                }
+                return this;
+            }
+        }).toString();
+    }
+    
+}
