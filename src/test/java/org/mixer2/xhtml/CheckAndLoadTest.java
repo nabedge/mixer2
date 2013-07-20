ccabe5e9c731c3c7d9c7bd977fc0326b84ff5397
nabedge <nabedge@gmail.com>
2013-07-20 23:30:19 +0900

    trying to get exception message...

--- src/test/java/org/mixer2/xhtml/CheckAndLoadTest.java
+++ src/test/java/org/mixer2/xhtml/CheckAndLoadTest.java
@@ -1,10 +1,14 @@
 package org.mixer2.xhtml;
 
 import java.io.File;
+import java.io.IOException;
+
+import javax.xml.bind.UnmarshalException;
 
 import org.apache.commons.lang.SystemUtils;
 import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.mixer2.Mixer2Engine;
 import org.mixer2.xhtml.exception.Mixer2JAXBException;
@@ -30,9 +34,33 @@ public class CheckAndLoadTest {
         }
     }
 
+    @Ignore
     @Test(expected = Mixer2JAXBException.class)
     public void exceptionThrowTest() throws Exception {
         m2e.checkAndLoadHtmlTemplate(new File(templateFilePath));
+    }
+
+    @Ignore
+    @Test
+    public void getMsgFromException() {
+        try {
+            m2e.checkAndLoadHtmlTemplate(new File(templateFilePath));
+        } catch (Mixer2JAXBException e) {
+            //e.printStackTrace();
+            Throwable linkedException = e.getLinkedException();
+            if (linkedException instanceof javax.xml.bind.UnmarshalException) {
+                UnmarshalException ue = (UnmarshalException) linkedException;
+                linkedException = ue.getLinkedException();
+                if (linkedException instanceof org.xml.sax.SAXParseException) {
+                    String msg = linkedException.getMessage();
+                    System.out.println(msg + "\n");
+                    linkedException.printStackTrace();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
     }
     
 }
