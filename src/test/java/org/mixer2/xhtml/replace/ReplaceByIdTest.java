b50253918b5e1d8e47fed78041993d13ebbb6c63
nabedge <nabedge@gmail.com>
2013-12-10 18:13:49 +0900

    bug fix on removeById() and replaceById()

--- src/test/java/org/mixer2/xhtml/replace/ReplaceByIdTest.java
+++ src/test/java/org/mixer2/xhtml/replace/ReplaceByIdTest.java
@@ -20,11 +20,14 @@ import org.mixer2.jaxb.xhtml.Div;
 import org.mixer2.jaxb.xhtml.Html;
 import org.mixer2.jaxb.xhtml.Li;
 import org.mixer2.jaxb.xhtml.Ol;
+import org.mixer2.jaxb.xhtml.Script;
 import org.mixer2.jaxb.xhtml.Span;
+import org.mixer2.jaxb.xhtml.Style;
 import org.mixer2.jaxb.xhtml.Table;
 import org.mixer2.jaxb.xhtml.Td;
 import org.mixer2.jaxb.xhtml.Tr;
 import org.mixer2.xhtml.Mixer2EngineSingleton;
+import org.mixer2.xhtml.TagCreator;
 import org.mixer2.xhtml.exception.TagTypeUnmatchException;
 
 /**
@@ -52,6 +55,32 @@ public class ReplaceByIdTest {
         } else {
             templateFilePath = templateFilePath.replaceFirst("file:", "");
         }
+    }
+    
+    @Test
+    public void style() throws Exception {
+        html = m2e.loadHtmlTemplate(new File(templateFilePath));
+        assertNotNull(html.getById("style1", Style.class));
+        Style style = TagCreator.styleWithId("style2");
+        style.setType("text/css");
+        style.setContent("foobar { color:red; }");
+        html.replaceById("style1", style);
+        assertNull(html.getById("style1", Style.class));
+        assertNotNull(html.getById("style2", Style.class));
+    }
+    
+    @Test
+    public void script() throws Exception {
+        html = m2e.loadHtmlTemplate(new File(templateFilePath));
+        assertNotNull(html.getById("fooScript", Script.class));
+        
+        Script barScript = TagCreator.scriptWithId("barScript");
+        barScript.setSrc("bar.js");
+        barScript.setType("text/javascript");
+
+        html.replaceById("fooScript", barScript);
+        assertNull(html.getById("fooScript", Script.class));
+        assertNotNull(html.getById("barScript", Script.class));
     }
 
     @Test
